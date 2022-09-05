5e6bc087d0a8cd0c189598f340d65aae7983e1a4
Apolyon6k <nbj@gmx.de>
2022-09-05 20:26:40 +0200

    added dragon sprite and changed enemy color to white...

--- PyGameTest01/PyGameTest01.py
+++ PyGameTest01/PyGameTest01.py
@@ -2,10 +2,15 @@ import pygame
 import random
 
 from pygame.locals import(
+    RLEACCEL,
     K_UP,
     K_DOWN,
     K_LEFT,
     K_RIGHT,
+    K_a,
+    K_s,
+    K_d,
+    K_w,
     K_ESCAPE,
     KEYDOWN,
     QUIT
@@ -17,18 +22,18 @@ screenHeight = 600
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75,25))
-        self.surf.fill((255,255,255))
+        self.surf = pygame.image.load("pix/Moving Forward_000.png").convert()
+        self.surf.set_colorkey((255,255,255),RLEACCEL)
         self.rect = self.surf.get_rect()
 
     def update(self,pressedKeys):
-        if pressedKeys[K_UP]:
+        if pressedKeys[K_UP] or pressedKeys[K_w]:
             self.rect.move_ip(0,-5)
-        if pressedKeys[K_DOWN]:
+        if pressedKeys[K_DOWN] or pressedKeys[K_s]:
             self.rect.move_ip(0,5)
-        if pressedKeys[K_LEFT]:
+        if pressedKeys[K_LEFT] or pressedKeys[K_a]:
             self.rect.move_ip(-5,0)
-        if pressedKeys[K_RIGHT]:
+        if pressedKeys[K_RIGHT] or pressedKeys[K_d]:
             self.rect.move_ip(5,0)
 
         if self.rect.left<0:
@@ -44,7 +49,7 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
         self.surf = pygame.Surface((20,10))
-        self.surf.fill((255,0,0))
+        self.surf.fill((255,255,255))
         self.rect = self.surf.get_rect(
             center=(
                 random.randint(screenWidth+20,screenWidth+100),
@@ -106,6 +111,8 @@ while running:
 
     #update screen
     pygame.display.flip()
+
+    #set framerate
     clock.tick(60)
 
 pygame.quit()
